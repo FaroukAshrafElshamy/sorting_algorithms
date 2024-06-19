dac9d8ab259876077fcf44048d25e639f433b68c
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-06-19 19:59:11 +0300

    Soft

--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
 #ifndef SORT_H
-#define SPRT_H
+#define SORT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
 
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
