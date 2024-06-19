3aa0e9bed1f4330f47b406af341e525dd4e810b8
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-06-19 19:53:27 +0300

    Sort

--- sort.h
+++ sort.h
@@ -1,12 +1,10 @@
-#ifdef SORT_H
+#ifndef SORT_H
 #define SPRT_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-
 
 #endif
